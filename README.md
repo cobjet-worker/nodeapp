445326555fd02552f54d3dd298e061cdd005ba27
Alex Davis <alex2006hw@gmail.com>
2015-01-29 01:05:15 -0800

    usinglocalmongo

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # nodeapp
 nodeapp agent
+- mongoose.connect('mongodb://user:password@ds043947.mongolab.com:43947/dev');
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var clientController = require('./controllers/client');
 var expressPort = 8888;
 
 // Connect to the cobjets MongoDB
-mongoose.connect('mongodb://rw:cobjetrw@ds043947.mongolab.com:43947/dev');
+mongoose.connect('mongodb://localhost:27017/dev');
 
 // Create our Express application
 var app = express();
